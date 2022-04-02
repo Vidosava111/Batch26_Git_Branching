3e4f743ce32ed0acb6b1f32709b3071311206a85
Vidosava111 <vidosavap1001@gmail.com>
2022-04-02 14:40:33 -0400

    updated test case TC002

--- src/test_cases/TC002.java
+++ src/test_cases/TC002.java
@@ -4,5 +4,9 @@ public class TC002 {
     public static void main(String[] args) {
         //Testing case 2
         // writing smoke tests...
+
+        // creating a report
+        // screenshots in failure
+
     }
 }
